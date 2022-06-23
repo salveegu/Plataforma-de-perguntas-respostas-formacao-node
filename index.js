6e5fba5dd329c906d341c99cab772832f703b718
Gustavo de Souza Santos <salveegu@gmail.com>
2022-06-23 15:19:21 -0300

    15:18 quinta-feira, 23 de junho de 2022 (BRT) Horário em Camboriú, SC

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const app = express();
+// body parser usado para processar dados enviados por meio de um corpo de solicitação HTTP . Ele expõe quatro middlewares expressos para análise de texto, JSON, codificação de URL e conjunto de dados brutos por meio de um corpo de solicitação HTTP.
+const bodyParser = require("body-parser");
 
 //definindo meu renderizador de HTML
 app.set('view engine','ejs');
 
 //criando a pasta para salvar os arquivos estaticos/imagens planilhas/css 
 app.use(express.static('public'));
+
+//configurando o body-parser
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
 
 app.locals.siteName = "Site do Gustavo"
 
@@ -22,7 +29,10 @@ app.get("/perguntar",(req,res)=>{
 
 //definição de rota 002
 app.post("/salvarpergunta",(req,res)=>{
-    res.send("Formulário recebido");
+    
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+    res.send("Formulário recebido! Titulo: "+titulo+ "Descricao: " + descricao);
 });
 
 app.listen(8080,()=>{
